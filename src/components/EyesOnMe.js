c62d01b19dfe2fca026eefd9ae72e25626aee8fd
Aidin Forooghi <80118914+aidinforooghi@users.noreply.github.com>
2023-06-18 17:51:43 -0400

    Update EyesOnMe.js

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <button onFocus={this.handleFocus} onBlur={this.handleBlur}>
+          Eyes on me
+        </button>
+      </div>
+    );
+  }
+
+  handleFocus = () => {
+    console.log('Good!');
+    // Additional logic can be added here
+  };
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+    // Additional logic can be added here
+  };
+}
+
+export default EyesOnMe;
